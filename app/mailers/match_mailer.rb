6d6446f49993d9aaab06bbee6871eebbff0506ef
Ignacio Rodrigo <irodrigo17@gmail.com>
2015-02-04 02:53:59 -0200

    Update mail URL

--- app/mailers/match_mailer.rb
+++ app/mailers/match_mailer.rb
@@ -3,7 +3,14 @@ class MatchMailer < ApplicationMailer
   def match_reminder_email(match, player)
     @match = match
     @player = player
-    @url = "http://localhost:3000/matches/#{match.id}/join?player=#{player.id}"
+
+    @url = url_for(
+      controller: 'matches',
+      action: 'join',
+      id: match.id,
+      player: player.id,
+      only_path: false
+    )
     mail(to: player.email, subject: 'Futbol 5')
   end
 
